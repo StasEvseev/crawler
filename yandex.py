9420cd45723b14a6385cbb184153d6def27316d7
unknown <skhairullina@atc14-66.at-consulting.ru>
2014-09-13 00:04:11 +0400

    Yesss!! First work!

--- yandex.py
+++ yandex.py
@@ -8,6 +8,7 @@ class Yandex(object):
         self._sep_params = '?'
         self._sep_prm = '&'
         self._serp_block_class = 'serp-block'
+        self._serp_item_class = 'serp-item'
 
     def create_request(self, query, deep, region_id):
         req = self._url + self._sep_params + 'lr=' + region_id + self._sep_prm + 'text=' + query
@@ -23,12 +24,32 @@ class Yandex(object):
         req = self.create_request(query, deep, region_id)
         self.fetch_data_local()
 
-        print self._g.doc.select('//div[contains(@class, "serp-block")]').exists()
+        #print self._g.doc.select('//div[contains(@class, "serp-block")]').exists()
         # print self._g.doc.select('//a[contains(@class, "b-link")]').text()
-        serp_block = self._g.doc.select('//div[contains(@class, "' + self._serp_block_class + '")]')
-        print serp_block.exists()
-        list = serp_block.select('//div[contains(@class, "serp-item")]').count()
-        print list
+        serp_items = self._g.doc.select('//div[contains(@class, "' + self._serp_block_class + '")]//div[contains(@class, "' + self._serp_item_class + '")]')
+
+        serp_it = self._g.doc.select('//div[contains(@class, "' + self._serp_block_class + '")]//div[contains(@class, "' + self._serp_item_class + '")]//a[contains(@class, "b-link serp-item__title-link")]')#.select('//a[contains(@class, "b-link serp-item__title-link")]')
+
+        print serp_items.count()
+        print serp_it.count()
+
+        for it in serp_it:
+            print it.text()
+
+        #print serp_items.exists()
+        #print serp_items.html()
+
+        #print serp_items[0].html()
+        #print serp_items[15].select('//a[contains(@class, "b-link serp-item__title-link")]').text()
+        #print serp_items.count()
+        #for item in serp_items:
+        #    print item.select('//a[contains(@class, "b-link serp-item__title-link")]').html()
+
+        #    pass
+            #print item.select('//a[contains(@class, "b-link")]').text()
+            #print item.html()
+        #print serp_block.select('//div[contains(@class, "' + self._serp_item_class + '")]').html()
+        #print list.select('//a[contains(@class, "b-link")]').html()
         #
-        for el in list:
-            print el.select('//a[contains(@class, "b-link")]')
+        #for el in list:
+        #    print el.select('//a[contains(@class, "b-link")]').html()
